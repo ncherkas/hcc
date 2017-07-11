954734934006e32b5543864652f981e09a3d9138
Nazar Cherkas <n.cherkas.ua@gmail.com>
2017-07-11 15:23:13 +0300

    Solution to the Hazelcast code challenge

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
-# hcc
+# Hazelcast code challenge
+
+## Problem
+```
+ Imagine an application that is deployed in 10 nodes. A node is a
+ separate JVM process, potentially can be running on different physical
+ machine.
+
+ Write an application that will be running on 10 nodes. The application
+ need to coordinate among the nodes and make sure that one and only one
+ of them does a System.out.println("We are started!"). Note: that not all
+ 10 nodes shall start at the same time.
+
+ It is possible that some will start couple of seconds/minutes later.
+ Some will not at all. Still I want this message to be printed and only
+ once. 
+ You can use an existing library for the solution. No need to build a distributed system from scratch. 
+
+ Providing an elegant solution to the problem is what we seek.
+```
+
+## Solution
+My solution is a kind of double-checked locking using Hazelcast distributed objects - Lock and ICountDownLatch. Depending on value of config parameter "app.cluster.waitToBecomeActive" (see application.properties) application either performs start logic (in our case outputs "We are started!") on a 1st instance that is up and acquired a lock or waits until number of instances set by config param "app.cluster.size" are up and runs start logic after it - on a arbitrary instance that has acquired a lock. By doing this we ensure that start logic will be executed once and only once.
+
+## Testing
+
+### 1. Build instructions
+Simply run ```maven clean install``` in the project directory.
+
+### 2. Unit tests
+Unit tests are checking two scenarios:
+ - cluster consisting of 10 instances running in parallel, start logic executed by the very first instance which is up and running and which has acquired a lock
+ - cluster of 3 instances, start logic gets executed when all 3 instances are up and running - by one of them which has acquired a lock
+In unit tests I've used ```TestHazelcastInstanceFactory``` which creates a mock communication protocol instead of tcp network.
+
+### 2. Docker
+Solution includes a build-in support to test it within Docker environment.
+Instruction to run application within Docker Compose cluster:
+ - install [Docker](https://www.docker.com/) 
+ - in project directory run run ```maven clean install```
+ - run ```mvn package docker:build -Dmaven.test.skip=true``` 
+ - if you're using Mac, export environment variable pointing to Docker host:
+```
+export DOCKER_HOST=unix:///var/run/docker.sock
+```
+ - export environment variables to override default application settings:
+```
+export DOCKER_HCC_CLUSTER_SIZE=10
+export DOCKER_HCC_WAIT=false
+```
+ - within docker directory run ```docker-compose up --scale hcc-app=$DOCKER_HCC_CLUSTER_SIZE -d``` - this will set up a cluster of 10 machines in detach mode
+ - wait a bit and run ```docker-compose logs | grep started!``` to search for the instance that executed start logic - it will be a single instance no matter what is the size of the cluster (I've tested max 50 containers)
+ - run ```docker-compose stop ``` to stop the cluster
+
---
+++ src/main/java/com/ncherkas/hazelcast/App.java
@@ -0,0 +1,122 @@
+package com.ncherkas.hazelcast;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+import static java.lang.Boolean.TRUE;
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+import com.google.common.annotations.VisibleForTesting;
+import com.hazelcast.config.Config;
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IAtomicReference;
+import com.hazelcast.core.ICountDownLatch;
+import com.hazelcast.core.ILock;
+import com.typesafe.config.ConfigFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Solution to the Hazelcast code challenge. It's a kind of double-checked locking using
+ * Hazelcast distributed objects - Lock and ICountDownLatch.
+ * Depending on value of config parameter "app.cluster.waitToBecomeActive" (see application.properties)
+ * application either performs start logic (in our case outputs "We are started!") on a 1st instance
+ * that is up and acquired a lock or waits until number of instances set by config param "app.cluster.size"
+ * are up and runs start logic after it - on a arbitrary instance that has acquired a lock.
+ */
+public class App {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
+
+  private final com.typesafe.config.Config appConfig;
+  private final HazelcastInstance hazelcastInstance;
+
+  /**
+   * App ctor.
+   * @param hazelcastInstance Hazelcast instance
+   * @param appConfig application config
+   */
+  public App(HazelcastInstance hazelcastInstance, com.typesafe.config.Config appConfig) {
+    this.hazelcastInstance = checkNotNull(hazelcastInstance);
+    this.appConfig = checkNotNull(appConfig);
+  }
+
+  /**
+   * Runs the application. Please see application.properties for default config.
+   * Note that config properties can be overrided using Java system properties.
+   */
+  public void run() {
+    try {
+      if (appConfig.getBoolean("app.cluster.waitToBecomeActive")) {
+        LOGGER.debug("Waiting for the cluster to become active...");
+        waitForClusterToBecomeActive();
+      }
+
+      LOGGER.debug("Starting instance {}", hazelcastInstance.getName());
+      start();
+
+      // Sleeping as if we are doing some important work
+      SECONDS.sleep(appConfig.getLong("app.sleepSec"));
+    } catch (InterruptedException ex) {
+      Thread.currentThread().interrupt();
+      throw new RuntimeException("Interrupted on application start", ex);
+    }
+  }
+
+  private void waitForClusterToBecomeActive() throws InterruptedException {
+    int clusterSize = appConfig.getInt("app.cluster.size");
+    ICountDownLatch clusterStartCountDownLatch = hazelcastInstance.getCountDownLatch("clusterStartCountDown");
+    clusterStartCountDownLatch.trySetCount(clusterSize);
+    clusterStartCountDownLatch.countDown();
+    LOGGER.debug("{} cluster instances active so far", clusterSize - clusterStartCountDownLatch.getCount());
+    clusterStartCountDownLatch.await(appConfig.getLong("app.cluster.waitTimeoutSec"), SECONDS);
+  }
+
+  private void start() throws InterruptedException {
+    IAtomicReference<Boolean> isStarted = hazelcastInstance.getAtomicReference("isStarted");
+
+    if (!TRUE.equals(isStarted.get())) {
+      ILock startLock = hazelcastInstance.getLock("startLock");
+      long lockWaitTimeSec = appConfig.getLong("app.lock.waitTimeSec");
+      long lockLeaseTimeSec = appConfig.getLong("app.lock.leaseTimeSec");
+
+      startLock.tryLock(lockWaitTimeSec, SECONDS, lockLeaseTimeSec, SECONDS);
+
+      try {
+        if (!TRUE.equals(isStarted.get())) {
+          signalWeAreStarted();
+          isStarted.set(TRUE);
+        }
+      } finally {
+        try {
+          startLock.unlock();
+        } catch (IllegalMonitorStateException ex) {
+          LOGGER.warn("Critical section guarantee can be broken");
+        }
+      }
+    }
+  }
+
+  @VisibleForTesting
+  void signalWeAreStarted() {
+    LOGGER.info("We are started!");
+  }
+
+  /**
+   * Application entry point.
+   * @param args args
+   */
+  public static void main(String[] args) {
+    com.typesafe.config.Config appConfig = ConfigFactory.load();
+    LOGGER.debug("Running with application config: {}", appConfig);
+
+    Config hazelcastConfig = new Config();
+
+    HazelcastInstance hazelcastInstance = Hazelcast.newHazelcastInstance(hazelcastConfig);
+
+    App app = new App(hazelcastInstance, appConfig);
+    app.run();
+
+    LOGGER.info("Stopping application...");
+    System.exit(0);
+  }
+}
